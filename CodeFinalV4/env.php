4aea6c17c37c25d1e12fc1c14f390012aaf6a56d
Poh000 <bierman.noa3@gmail.com>
2025-11-24 16:38:31 +0100

    Ajout de charlie

---
+++ CodeFinalV4/env.php
@@ -0,0 +1,8 @@
+<?php
+
+    const BDD_LOGIN = "root";
+    const BDD_PASSWORD = "root";
+    const BDD_SERVER = "localhost";
+    const BDD_NAME = "mythicscans";
+    const BASE_URL = "";
+?>
---
+++ CodeFinalV4/public/js/main.js
@@ -0,0 +1,54 @@
+// Récupère les constantes 
+const carousel = document.querySelector('.carousel'); // Récupère la classe carousel
+const items = [...carousel.children]; // Récupère ce qu'il y a dans le carousel
+const totalItems = items.length; // Compte le nombre d'items dans le carousel
+let itemWidth; // Définit la largeur d'un item en fonction du breakpoint
+
+if (window.innerWidth <= 576) {
+  itemWidth = 70;
+} else if (window.innerWidth <= 768) {
+  itemWidth = 100;
+} else {
+  itemWidth = 160;
+}
+
+
+let positions = []; // Crée un tableau pour stocker les positions
+
+items.forEach((item, i) => {
+  positions[i] = i * itemWidth; // Position horizontale d'un item
+  item.style.left = positions[i] + 'px'; // Applique la position au style left pour décaler l'image
+  item.style.position = 'absolute'; // Ajoute position absolute à l'item pour que le style fonctionne
+});
+
+function deplacer() {
+  items.forEach((item) => {
+    item.style.transition = 'left 0.5s ease'; // Ajoute une transition pour le déplacement à gauche
+  });
+
+  for (let i = 0; i < totalItems; i++) {
+    positions[i] -= itemWidth; // Décrémente sa position pour le décaler d'un cran à gauche
+    items[i].style.left = positions[i] + 'px'; // Modifie sa position
+  }
+
+  setTimeout(() => {
+    for (let i = 0; i < totalItems; i++) {
+      if (positions[i] < -itemWidth) {
+        items[i].classList.add('hidden'); // Cache l'image pour la renvoyer à la fin sans que cela se voie
+
+        const maxPos = Math.max(...positions); // Calcule la position maximale de tous les éléments
+        positions[i] = maxPos + itemWidth;
+
+        items[i].style.transition = 'none';
+        items[i].style.left = positions[i] + 'px'; // On le replace à la fin
+
+        setTimeout(() => {
+          items[i].classList.remove('hidden'); // Affiche à nouveau l'item
+        }, 20);
+      }
+    }
+  }, 600);
+}
+setInterval(deplacer, 2000);
+
+
